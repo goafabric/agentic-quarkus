dee22660af8816c90515d571876546a5c2b2d1cc
Andreas Mautsch <amautsch>
2026-09-01 07:27:39 +0200

    chang

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ tasks.withType<KotlinCompile>().all { compilerOptions { jvmTarget.set(JvmTarget.
 val dockerRegistry = "goafabric"
 plugins {
 	jacoco
-	id("io.quarkus") version "3.38.2"
+	id("io.quarkus") version "3.39.1"
 	id("net.researchgate.release") version "3.1.0"
 
 	kotlin("jvm") version "2.4.0"
@@ -24,8 +24,8 @@ dependencies {
 		testImplementation("com.tngtech.archunit:archunit-junit5:1.4.2")
 	}
 
-	implementation(enforcedPlatform("io.quarkus:quarkus-bom:3.38.2"))
-	implementation(enforcedPlatform("io.quarkus.platform:quarkus-langchain4j-bom:3.38.2"))
+	implementation(enforcedPlatform("io.quarkus:quarkus-bom:3.39.1"))
+	implementation(enforcedPlatform("io.quarkus.platform:quarkus-langchain4j-bom:3.39.1"))
 }
 dependencies {
 	//web
